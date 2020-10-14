c0f2b819530cd84414104a28831d165229926e20
mnashrullah <muhammadnashrullah.it@gmail.com>
2020-10-14 16:20:30 +0700

    Initial Commit

---
+++ BK/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BK
+//
+//  Created by Muhammad Nashrullah on 14/10/20.
+//  Copyright © 2020 BK. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
