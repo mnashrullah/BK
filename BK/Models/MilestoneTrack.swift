b01e98644a81aa6c03efecb7e7a72e9822e88502
hilmyghozy <hilmyghozy11@gmail.com>
2020-11-02 12:13:27 +0700

    milestone screen

---
+++ BK/Models/MilestoneTrack.swift
@@ -0,0 +1,21 @@
+//
+//  MilestoneTrack.swift
+//  mila
+//
+//  Created by hilmy ghozy on 01/11/20.
+//  Copyright © 2020 BK. All rights reserved.
+//
+
+import SwiftUI
+
+struct MilestoneTrack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MilestoneTrack_Previews: PreviewProvider {
+    static var previews: some View {
+        MilestoneTrack()
+    }
+}
